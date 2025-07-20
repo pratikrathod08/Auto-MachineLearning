eeea38992786a0765dabf9bb928305320a324266
Pratikrathod08 <pratikr1521998@gmail.com>
2025-07-20 13:17:54 +0530

    final functionality added for data preprocessing automation

--- auto_machinelearning/ml/preprocessor/preprocess.py
+++ auto_machinelearning/ml/preprocessor/preprocess.py
@@ -1,22 +1,27 @@
 import os, sys 
-
+import joblib 
 import numpy as np 
 import pandas as pd 
 
 from auto_machinelearning.ml.preprocessor.datareader import ReadData
 from auto_machinelearning.ml.preprocessor.datadescriber import DataDescribe
 from auto_machinelearning.ml.preprocessor.auto_preprocess import AutoPreprocessor
+from auto_machinelearning.ml.preprocessor.processor import Preprocess
+
 
 class AutoPreprocess: 
-    def __init__(self, path: str, file_type: str, store_path: str, target_column: str, **params): 
+    def __init__(self, path: str, file_type: str, store_path: str, preprocessor_store_path: str,  target_column: str, **params): 
         self.path = path
         self.file_type = file_type
         self.store_path = store_path
         self.target_column = target_column
+        self.preprocessor_store_path = preprocessor_store_path
         self.params = params
         self.df = None
         self.description = None
         self.preprocessed_df = None
+        self.encoder = None
+        self.scaler = None
 
     def read_file(self):
         read_data = ReadData()
@@ -31,23 +36,14 @@ class AutoPreprocess:
         data_describer.summarize()
         description = data_describer.get_summary_dict()
         return description
-    
-    def preprocess_data(self, df: pd.DataFrame, target_column: str): 
-        data_processor = AutoPreprocessor(df, target_column)
-        processed_df = data_processor.run_preprocessing()
-        return processed_df
-    
-    def store_df(self): 
-        if self.preprocessed_df is not None:
-            dir_path = os.path.dirname(self.store_path)
-            os.makedirs(dir_path, exist_ok=True)
-            self.preprocessed_df.to_csv(self.store_path, index=False)
-        else:
-            raise ValueError("Preprocessed dataframe is None.")
+
+    def preprocess_data(self): 
+        data_processor = Preprocess(self.df, self.target_column, self.store_path, self.preprocessor_store_path)
+        self.preprocessed_df =  data_processor.fit()
+        return self.preprocessed_df
 
     def process(self): 
         self.df = self.read_file()
         self.description = self.describe_data(self.df, self.target_column)
-        self.preprocessed_df = self.preprocess_data(self.df, self.target_column)
-        self.store_df()
+        self.preprocessed_df = self.preprocess_data()
         return self.preprocessed_df

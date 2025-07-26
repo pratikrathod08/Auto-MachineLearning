1b42cf6eaa4780f53dfca79e7fb1aef7317bd215
Pratikrathod08 <pratikr1521998@gmail.com>
2025-07-26 14:40:24 +0530

    done correction in read me file

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ def get_requirements(file_path:str)->List[str]:
 
 setup(
     name='Auto-MachineLearning',
-    version='0.0.9',
+    version='0.1.0',
     author='PratikRathod',
     author_email='pratikr1521998@gmail.com',
     description="An AutoML framework with classification, regression, etc.",
